c5fab4540a9c9f96d26ed63a60eb3127d7a5ac6b
ymsk.sky <ymsk.sky.95@gmail.com>
2020-01-10 22:56:35 +0900

    3種類のサジェストを実装/動作確認

--- _vue/js/main.js
+++ _vue/js/main.js
@@ -1,6 +1,10 @@
 var app = new Vue({
   el: "#app",
   data: {
-    message: "Vue.js"
+    message: "Vue.js",
+    selected_simple: null,
+    simpleSuggestionList: ['バナナ', 'りんご', 'オレンジ'],
+    selected_cool: null,
+    items: ['Alice', 'Bob', 'Chris', 'David', 'Erm']
   }
 })
