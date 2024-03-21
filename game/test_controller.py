a5c29f76f67699492d6d13e7e53312ca20ca312c
ymsk-sky <ymsk.sky.95@gmail.com>
2024-03-21 21:45:45 +0900

    game update

--- game/test_controller.py
+++ game/test_controller.py
@@ -2,9 +2,9 @@
 # import os
 # os.environ['SDL_JOYSTICK_HIDAPI_PS4_RUMBLE'] = '1'
 
-import time
 import pygame
 
+pygame.init()
 pygame.joystick.init()
 if pygame.joystick.get_count() < 1:
     print("No controllers")
@@ -13,4 +13,12 @@ joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
 joystick.rumble(1.0, 1.0, 0)
-time.sleep(10)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONUP:
+            running = False
+
+joystick.stop_rumble()
+print("fin")
